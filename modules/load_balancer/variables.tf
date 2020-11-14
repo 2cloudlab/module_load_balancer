7715af046cd463e1712c84ba8b90fb244698ab2e
2cloudlab <59782420+2cloudlab@users.noreply.github.com>
2020-11-14 10:22:37 +0800

    Create variables.tf

---
+++ modules/load_balancer/variables.tf
@@ -0,0 +1,20 @@
+
+variable "download_url" {
+  type        = string
+}
+
+variable "package_base_dir" {
+  type        = string
+}
+
+variable "app_dir" {
+  type        = string
+}
+
+variable "wsgi_app" {
+  type        = string
+}
+
+variable "envs" {
+  type        = list
+}
